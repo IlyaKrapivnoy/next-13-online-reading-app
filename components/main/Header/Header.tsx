a1e27c7db185da3698280b0c439a8101d14fef3e
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-05 17:32:53 +0100

    add arrow to active link

--- components/main/Header/Header.tsx
+++ components/main/Header/Header.tsx
@@ -2,8 +2,9 @@
 
 import React, { ReactNode, useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
-import Menu from '../../../public/menu-icon.png';
-import Close from '../../../public/cross-icon.png';
+import MenuIcon from '../../../public/menu-icon.png';
+import CloseIcon from '../../../public/cross-icon.png';
+import ArrowIcon from '../../../public/arrow-icon.png';
 import Link from 'next/link';
 import { navigation } from '@/data/header';
 import { usePathname } from 'next/navigation';
@@ -32,6 +33,8 @@ const Header: React.FC<HeaderProps> = ({ logo, logoWidth, headerMoto }) => {
     toggleBodyOverflow(false);
   };
 
+  const isActiveLink = (href: string): Boolean => pathName === href;
+
   return (
     <header className='flex justify-between items-center min-h-[100px] border-b-[6px] border-black'>
       <div className='w-1/5'>
@@ -43,7 +46,7 @@ const Header: React.FC<HeaderProps> = ({ logo, logoWidth, headerMoto }) => {
       {!isMenuOpen ? (
         <div className='w-1/5 flex justify-end'>
           <Image
-            src={Menu}
+            src={MenuIcon}
             width={30}
             onClick={openMobileMenu}
             alt='menu icon'
@@ -53,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ logo, logoWidth, headerMoto }) => {
       ) : (
         <>
           <Image
-            src={Close}
+            src={CloseIcon}
             width={30}
             onClick={closeMobileMenu}
             alt='menu icon'
@@ -64,19 +67,31 @@ const Header: React.FC<HeaderProps> = ({ logo, logoWidth, headerMoto }) => {
               'absolute top-0 bottom-0 right-0 left-0 bg-gray-50 flex justify-center items-center'
             }
           >
-            <nav className='flex flex-col text-2xl font-black'>
+            <nav className='relative flex flex-col text-2xl font-black'>
               {navigation.map(
                 ({ href, name }: { href: string; name: string }): ReactNode => (
-                  <Link
-                    key={name}
-                    href={href}
-                    className={`hover:text-gray-600 ${
-                      pathName === href ? 'text-red-500' : ''
-                    }`}
-                    onClick={closeMobileMenu}
-                  >
-                    {name}
-                  </Link>
+                  <div className='flex items-center my-2' key={name}>
+                    <Image
+                      src={ArrowIcon}
+                      width={26}
+                      alt='active link icon'
+                      className={`${
+                        isActiveLink(href) ? 'opacity-100' : 'opacity-0'
+                      } mr-4`}
+                    />
+                    <Link
+                      key={name}
+                      href={href}
+                      className={`hover:text-gray-600 ${
+                        isActiveLink(href)
+                          ? 'text-red-500 hover:text-red-500'
+                          : ''
+                      }`}
+                      onClick={closeMobileMenu}
+                    >
+                      {name}
+                    </Link>
+                  </div>
                 )
               )}
             </nav>
